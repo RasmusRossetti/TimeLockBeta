807f5de196677a3d9dc0f0f49ce175512a7305e8
rasmus rossetti <rasmus.rossetti@hotmail.com>
2022-11-16 13:22:59 +0100

    try fix bug

--- src/components/loadercomponents/SecondLoader.js
+++ src/components/loadercomponents/SecondLoader.js
@@ -2,41 +2,41 @@ import React from "react";
 
 const SecondLoader = () => {
   return (
-    <div class='border-spacing-6  shadow-lg rounded-md p-4 w-64  '>
-      <div class='animate-pulse flex space-x-4'>
-        <div class='flex-1 space-y-6 py-1'>
-          <div class='h-2 bg-slate-700 rounded'></div>
-          <div class='space-y-3'>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+    <div className='border-spacing-6  shadow-lg rounded-md p-4 w-64  '>
+      <div className='animate-pulse flex space-x-4'>
+        <div className='flex-1 space-y-6 py-1'>
+          <div className='h-2 bg-slate-700 rounded'></div>
+          <div className='space-y-3'>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
             </div>
-            <div class='h-2 bg-slate-700 rounded'></div>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+            <div className='h-2 bg-slate-700 rounded'></div>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
 
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
             </div>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
 
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
             </div>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
             </div>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
 
-              <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+              <div className='h-2 bg-slate-700 rounded col-span-1'></div>
             </div>
-            <div class='h-2 bg-slate-700 rounded'></div>
+            <div className='h-2 bg-slate-700 rounded'></div>
           </div>
         </div>
       </div>
